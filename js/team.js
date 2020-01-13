6f5e1768f3151f585fdda06a559cd9cb597b8905
leva84 <denfrut8@gmail.com>
2020-01-13 19:45:47 +0300

    Button:hover and js

---
+++ js/team.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+  document.getElementById('slogan1').onclick = function(){
+    alert('Привет!)');
+  };
+};
+
+window.onload = function(){
+  document.getElementById('slogan2').onclick = function(){
+    alert('Привет!)');
+  };
+};
+
+window.onload = function(){
+  document.getElementById('slogan3').onclick = function(){
+    alert('Привет!)');
+  };
+};
+
+window.onload = function(){
+  document.getElementById('slogan4').onclick = function(){
+    alert('Привет!)');
+  };
+};
