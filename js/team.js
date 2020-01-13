fadba8d6f694b1bd74b9024e5f07eb2411a7e587
leva84 <denfrut8@gmail.com>
2020-01-13 23:10:03 +0300

    Parallax intro

--- js/team.js
+++ js/team.js
@@ -1,17 +1,6 @@
-window.onload = function(){
-  document.getElementById('slogan1').onclick = function(){
-    alert('Привет!)');
-  };
-
-  document.getElementById('slogan2').onclick = function(){
-    alert('Привет!)');
-  };
-
-  document.getElementById('slogan3').onclick = function(){
-    alert('Привет!)');
-  };
-
-  document.getElementById('slogan4').onclick = function(){
-    alert('Привет!)');
-  };
-};
+$(function(){
+  $('.slogan-position').hide();
+  $('.position').click(function(){
+    $(this).next().slideToggle({duration: 500})
+  });
+});
